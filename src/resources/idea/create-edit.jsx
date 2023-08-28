4e32a42c677e16a97bd8a4bfd74171e0373f01b5
LorenzoJokhan <lorenzoluk@gmail.com>
2023-08-28 12:09:17 +0200

    Add budget field to idea in react admin beta

--- src/resources/idea/create-edit.jsx
+++ src/resources/idea/create-edit.jsx
@@ -61,6 +61,8 @@ const Form = (props) => (
       <TextInput source="title" variant="outlined" fullWidth/>
       <TextInput source="summary" options={{ multiLine: true }} variant="outlined" fullWidth validate={[required()]} />
       <TextInput multiline source="description" variant="outlined" fullWidth validate={[required()]}/>
+      <TextInput source="budget" variant="outlined" fullWidth/>
+
       <ReferenceArrayInput label="tags" source="tags" reference="tag" variant="outlined">
         <SelectArrayInput optionText="name"/>
       </ReferenceArrayInput>
